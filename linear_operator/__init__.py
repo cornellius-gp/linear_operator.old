93f35a0eeeb93c36e8dadb1e6670cb7403a5e6f2
Jake Gardner <jacobrg@seas.upenn.edu>
2020-11-12 16:30:40 -0500

    Add proper __init__ file

--- linear_operator/__init__.py
+++ linear_operator/__init__.py
@@ -1,3 +1,48 @@
 #!/usr/bin/env python3
+from . import (
+    lazy,
+    settings,
+    utils,
+)
+
+from .functions import (  # Deprecated
+    add_diag,
+    add_jitter,
+    dsmm,
+    inv_matmul,
+    inv_quad,
+    inv_quad_logdet,
+    log_normal_cdf,
+    logdet,
+    matmul,
+    root_decomposition,
+    root_inv_decomposition,
+)
+from .lazy import cat, delazify, lazify
 
 __version__ = "0.0.1"
+
+__all__ = [
+    # Submodules
+    "lazy",
+    "utils",
+    # Functions
+    "add_diag",
+    "add_jitter",
+    "cat",
+    "delazify",
+    "dsmm",
+    "inv_matmul",
+    "inv_quad",
+    "inv_quad_logdet",
+    "lazify",
+    "logdet",
+    "log_normal_cdf",
+    "matmul",
+    "root_decomposition",
+    "root_inv_decomposition",
+    # Context managers
+    "settings",
+    # Other
+    "__version__",
+]
