db00d44e89c41a2bd5d6fcccc5983db7167201bd
Max Balandat <balandat@fb.com>
2021-01-15 07:31:39 -0800

    Fall back for other funcitons if necesary, set _diag_is_constant flag

--- linear_operator/operators/kronecker_product_added_diag_linear_operator.py
+++ linear_operator/operators/kronecker_product_added_diag_linear_operator.py
@@ -24,28 +24,33 @@ class KroneckerProductAddedDiagLinearOperator(AddedDiagLinearOperator):
             raise RuntimeError(
                 "One of the LinearOperators input to AddedDiagLinearOperator must be a DiagLinearOperator!"
             )
+        self._diag_is_constant = isinstance(self.diag_tensor, ConstantDiagLinearOperator)
 
     def inv_quad_logdet(self, inv_quad_rhs=None, logdet=False, reduce_inv_quad=True):
-        # we want to call the standard InvQuadLogDet to easily get the probe vectors and do the
-        # solve but we only want to cache the probe vectors for the backwards
-        inv_quad_term, _ = super().inv_quad_logdet(
-            inv_quad_rhs=inv_quad_rhs, logdet=False, reduce_inv_quad=reduce_inv_quad
-        )
-        logdet_term = self._logdet() if logdet else None
-        return inv_quad_term, logdet_term
+        if self._diag_is_constant:
+            # we want to call the standard InvQuadLogDet to easily get the probe vectors and do the
+            # solve but we only want to cache the probe vectors for the backwards
+            inv_quad_term, _ = super().inv_quad_logdet(
+                inv_quad_rhs=inv_quad_rhs, logdet=False, reduce_inv_quad=reduce_inv_quad
+            )
+            logdet_term = self._logdet() if logdet else None
+            return inv_quad_term, logdet_term
+        return super().inv_quad_logdet(inv_quad_rhs=inv_quad_rhs, logdet=logdet, reduce_inv_quad=reduce_inv_quad)
 
     def _logdet(self):
-        # symeig requires computing the eigenvectors so that it's differentiable
-        evals, _ = self.linear_operator.symeig(eigenvectors=True)
-        evals_plus_diag = evals + self.diag_tensor.diag()
-        return torch.log(evals_plus_diag).sum(dim=-1)
+        if self._diag_is_constant:
+            # symeig requires computing the eigenvectors so that it's differentiable
+            evals, _ = self.linear_operator.symeig(eigenvectors=True)
+            evals_plus_diag = evals + self.diag_tensor.diag()
+            return torch.log(evals_plus_diag).sum(dim=-1)
+        return super()._logdet()
 
     def _preconditioner(self):
         # solves don't use CG so don't waste time computing it
         return None, None, None
 
     def _solve(self, rhs, preconditioner=None, num_tridiag=0):
-        if isinstance(self.diag_tensor, ConstantDiagLinearOperator):
+        if self._diag_is_constant:
             # we do the solve in double for numerical stability issues
             # TODO: Use fp64 registry once #1213 is addressed
 
@@ -67,10 +72,10 @@ class KroneckerProductAddedDiagLinearOperator(AddedDiagLinearOperator):
 
         # TODO: implement woodbury formula for non-constant Kronecker-structured diagonal operators
 
-        return super()._solve(rhs, preconditioner=None, num_tridiag=0)
+        return super()._solve(rhs, preconditioner=preconditioner, num_tridiag=num_tridiag)
 
     def _root_decomposition(self):
-        if isinstance(self.diag_tensor, ConstantDiagLinearOperator):
+        if self._diag_is_constant:
             # we can be use eigendecomposition and shift the eigenvalues
             evals, q_matrix = self.linear_operator.symeig(eigenvectors=True)
             updated_evals = DiagLinearOperator((evals + self.diag_tensor.diag()).pow(0.5))
@@ -78,7 +83,7 @@ class KroneckerProductAddedDiagLinearOperator(AddedDiagLinearOperator):
         return super()._root_decomposition()
 
     def _root_inv_decomposition(self, initial_vectors=None):
-        if isinstance(self.diag_tensor, ConstantDiagLinearOperator):
+        if self._diag_is_constant:
             evals, q_matrix = self.linear_operator.symeig(eigenvectors=True)
             inv_sqrt_evals = DiagLinearOperator((evals + self.diag_tensor.diag()).pow(-0.5))
             return MatmulLinearOperator(q_matrix, inv_sqrt_evals)
